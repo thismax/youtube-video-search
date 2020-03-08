ea1574b59e2f52bbb61096726d4384802974af77
thismax <max.g.jacobs@gmail.com>
2020-03-08 15:10:42 -0700

    Updates

---
+++ precache-manifest.8ced9701a41cc4c676d229cb89b3c6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '674b9f5759e7b8d00f4236a72cd575e9',
+    url: '/index.html',
+  },
+  {
+    revision: '2ea989fcde791e80de58',
+    url: '/static/css/main.328577a1.chunk.css',
+  },
+  {
+    revision: 'd5fb3607844fae95ac5a',
+    url: '/static/js/2.4c51f53f.chunk.js',
+  },
+  {
+    revision: '5356fa2f66e46e6c05e4cbe319ac7f1d',
+    url: '/static/js/2.4c51f53f.chunk.js.LICENSE.txt',
+  },
+  {
+    revision: '2ea989fcde791e80de58',
+    url: '/static/js/main.db3355bf.chunk.js',
+  },
+  {
+    revision: '6873f6dc275bdbb33640',
+    url: '/static/js/runtime-main.af8ca10c.js',
+  },
+]);
